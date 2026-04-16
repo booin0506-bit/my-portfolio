f5e19a4bafa049f33a6900db2fb4879460f40782
booin0506-bit <booin0506@gmail.com>
2026-04-16 19:06:41 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -197,17 +197,18 @@ const App = () => {
       category: 'UI/UX Design · Personal',
       description: '사용자의 일상 속 불편함을 해결하기 위해 새롭게 기획하고 구상 중인 모바일 앱 서비스 화면입니다.',
       tools: ['Figma', 'Protopie'],
-      imageUrl: '/img/portfolio1_1.jpg',
+      // 맨 앞에 점(.)을 추가하여 상대경로로 변경!
+      imageUrl: './img/portfolio1_1.jpg',
       detailImages: [
-        '/img/portfolio1_1.jpg',
-        '/img/portfolio1_2.jpg',
-        '/img/portfolio1_3.jpg',
-        '/img/portfolio1_4.jpg',
-        '/img/portfolio1_5.jpg',
-        '/img/portfolio1_6.jpg',
-        '/img/portfolio1_7.jpg',
-        '/img/portfolio1_8.jpg',
-        '/img/portfolio1_9.jpg'
+        './img/portfolio1_1.jpg',
+        './img/portfolio1_2.jpg',
+        './img/portfolio1_3.jpg',
+        './img/portfolio1_4.jpg',
+        './img/portfolio1_5.jpg',
+        './img/portfolio1_6.jpg',
+        './img/portfolio1_7.jpg',
+        './img/portfolio1_8.jpg',
+        './img/portfolio1_9.jpg'
       ],
       color: 'bg-purple-50'
     },
